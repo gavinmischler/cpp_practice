2cfe146dfae1914406db6f622b40bf3a0b18f492
Gavin <gavinmischler@gmail.com>
2018-04-26 01:11:52 -0400

    Encrypter code

---
+++ encrypter.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+
+string encodeLine(string &message)
+{
+  for (int i = 0; i < message.length(); i++)
+  {
+    if (i%2)
+    {
+      message[i]++;
+    } else
+    {
+      message[i] += 4;
+    }
+  }
+  return message;
+}
+
+string decodeLine(string &encodedmessage)
+{
+  for (int i = 0; i < encodedmessage.length(); i++)
+  {
+    if (i%2)
+    {
+      encodedmessage[i]--;
+    } else
+    {
+      encodedmessage[i] -= 4;
+    }
+  }
+  return encodedmessage;
+}
+
+int main()
+{
+  ifstream in;
+  string filename;
+  in.open("test.txt");
+  if (in.fail())
+  {
+  	cout << ("Couldn't find read text file");
+  	return 1;
+  }
+  //Encrypt each line and output to file
+  ofstream outf ("EncryptedMessage.txt");
+  while (in.peek() != EOF)
+  {
+  	string lineoftext;
+  	getline(in, lineoftext);
+    string encodedline = encodeLine(lineoftext);
+    outf << encodedline << "\n";
+  }
+  in.close();
+  outf.close();
+
+  //Decrypt each line and output to a different file
+  in.open("EncryptedMessage.txt");
+  if (in.fail())
+  {
+    cout << ("Couldn't find read enctryped message file") << "\n";
+  	return 1;
+  }
+
+  ofstream outf2("DecryptedMessage.txt");
+  while (in.peek() != EOF)
+  {
+  	string lineofencodedtext;
+  	getline(in, lineofencodedtext);
+    string decodedline = decodeLine(lineofencodedtext);
+    outf2 << decodedline << "\n";
+  }
+  in.close();
+  outf2.close();
+
+  return 0;
+
+
+}
